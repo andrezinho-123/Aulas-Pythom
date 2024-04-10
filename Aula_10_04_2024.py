27dcf3785cba0b6dec22050c7880321a07cb0f99
Andrezinho-123 <andre.computacaografica@gmail.com>
2024-04-10 20:43:31 -0300

    Aula While

---
+++ Aula_10_04_2024.py
@@ -0,0 +1,24 @@
+x = 1
+
+while x <= 100:
+    print(x)
+    x+=1
+x = 50
+
+while x <= 100:
+    print(x)
+    x+=1
+from time import sleep
+
+x = 10
+while x >= 0:
+    print(x)
+    time.sleep(1)
+    x -= 1
+
+print("Fogo!")
+
+Inicio = int(input("Entre com o primeiro número:"))
+fim = int(input("Entre com o último número:"))
+x = 0
+while x <=
