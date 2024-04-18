098bac2ac5d12a55108bf1094e78fb912494ff7a
Andrezinho-123 <andre.computacaografica@gmail.com>
2024-04-17 21:01:08 -0300

    Aula While 2

--- Aula_17_04_2024.py
+++ Aula_17_04_2024.py
@@ -1,21 +1,20 @@
-pontos = 0
-questao = 1
 
-while questao<= 3:
-    resposta = input(f"Resposta da questão{questao}:")
-    if questao == 1 and resposta =="b":
-        pontos = pontos + 1
-    elif questao == 2 and resposta =="a":
-        pontos = pontos + 1
-    elif questao == 3 and resposta == "d":
-        pontos = pontos + 1
-    questao = questao + 1
-print(f"O aluno fez{pontos}ponto(s).")
+cont = 1
+soma = 0
+while cont <= 10:
+ num = float(input(f"Digite o {cont}° número: "))
+ soma += num
+ cont += 1
 
-n = 1 #contador
-soma = 0 #acumulador
-while n <=10:
- x = float (input(f"Digite o número{n}:"))
- soma = soma + x
- n = n + 1
- print(f"Soma = {soma}")
+print(f"A soma dos 10 números é igual a {soma:.2f}.")
+print(f"A média dos 10 números é igual a {soma/10:.2f}.")
+print(f"A média dos 10 números é igual a {soma/(cont-1):.2f}.")
+
+
+soma=0
+while True:
+    num = input(f"Digite o numero ou escreva fim para somar: ")
+    if num == "fim":
+        break
+    soma += float(num)
+print("Resultado:" , soma)
