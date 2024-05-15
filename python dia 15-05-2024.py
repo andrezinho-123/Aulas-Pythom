45d798d5156be02b1831c532604f6f8c7feaab7d
Andrezinho-123 <andre.computacaografica@gmail.com>
2024-05-15 20:37:18 -0300

    aula de lista

---
+++ python dia 15-05-2024.py
@@ -0,0 +1,26 @@
+nomes_alunos = ["Mayene", "Pedro", "André", "Eduardo"]
+print(nomes_alunos)
+print(nomes_alunos[2])
+nomes_alunos[2]= "André"
+print(nomes_alunos)
+nomes_alunos.sort()
+print(nomes_alunos)
+idades = [21, 19, 37, 18, 0]
+idades.sort()
+print(idades)
+idades.sort(reverse=True)
+print(idades)
+idades[4] = 45
+print(idades)
+nomes_alunos.append("João")
+print(nomes_alunos)
+idades.append(19)
+print(idades)
+nomes_alunos.append(["Lucca" , "Lucas"])
+print(nomes_alunos)
+print(nomes_alunos[5])
+print(len(nomes_alunos))
+print(nomes_alunos[5][0])
+print(nomes_alunos_back)
+del nomes_alunos_back[2]
+print(nomes_alunos_back)
